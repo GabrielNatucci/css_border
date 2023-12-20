0c9238dadde4501d1d7af88c78526eb14370146a
GabrielNatucci <gabriel.natuccirusso@hotmail.com>
2023-12-20 16:16:26 -0300

    Todas funcoes prometidas adiconadas
    - funcao para resetar os quatro valores adicionada
    - funcao para copiar string do border-radius para clipboard

--- js/main_script.js
+++ js/main_script.js
@@ -1,14 +1,17 @@
-let inputs = document.getElementsByClassName("selectors")
+let inputs = document.getElementsByClassName("inputs")
 let square = document.getElementById("square");
 let reset = document.getElementById("reset");
-let metrics = document.getElementById("metrics");
+let metrics = document.getElementById("metrics-id");
+let cp = document.getElementById("clipboard");
 let metric = metrics.value + " "
 let borders = [0, 0, 0, 0];
+let border_string;
+
 
 let c_input;
 
 function updateSquare() { // atualiza o quadrado do preview com os valores atuais na variavel border
-    let border_string = borders[0] + metric + borders[1] + metric + borders[3] + metric + borders[2] + metric;
+    border_string = borders[0] + metric + borders[1] + metric + borders[3] + metric + borders[2] + metric;
     square.style.borderRadius = border_string;
     // console.log(border_string); // debug
 }
@@ -17,7 +20,12 @@ function updateBorders(value, input) { // atualiza a variavel border com o valor
     borders[input] = value;
 }
 
-for (let input in inputs) {
+metrics.addEventListener('input', function() { // atualiza variavel metric com o valor que o usuario selecionar
+    metric = metrics.value + " "
+    updateSquare();
+})
+
+for (let input in inputs) { // funcao que cuida dos inputs que controlam as bordas do quadrado
     if (input == "item") break;
 
     borders[input] = inputs[input].value;
@@ -33,10 +41,10 @@ for (let input in inputs) {
     })
 }
 
-metrics.addEventListener('input', function() { // atualiza variavel metric com o valor que o usuario selecionar
-    metric = metrics.value + " "
-    updateSquare();
+cp.addEventListener('click', function() { // event listener para copiar os dados para clipboard do user
+    navigator.clipboard.writeText("border-radius: " + border_string.trim() + ";");
 })
+
 
 reset.addEventListener('click', function() { // reseta os valores
     for (let input in inputs) {
